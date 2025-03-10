5c19d7d77e2d3646dd4cc107ed810f10bbbb575d
Jonatas Arão de Carvalho Neves <51247074+JonatasArao@users.noreply.github.com>
2025-03-10 16:30:12 -0300

    mobileModule - calculator_app - functional keyboard

--- mobileModule00/calculator_app/lib/main.dart
+++ mobileModule00/calculator_app/lib/main.dart
@@ -1,245 +1,387 @@
 import 'package:flutter/material.dart';
 
-void main()
-{
-	runApp(const CalculatorApp());
+void main() {
+  runApp(const CalculatorApp());
 }
 
-class CalculatorApp extends StatelessWidget
-{
-	const CalculatorApp({super.key});
+class CalculatorApp extends StatelessWidget {
+  const CalculatorApp({super.key});
 
-	@override
-	Widget build(BuildContext context)
-	{
-		return MaterialApp(
-			title: 'Calculator App',
-			theme: ThemeData(
-				colorScheme: ColorScheme.fromSeed(
-					seedColor: Colors.blueGrey,
-					brightness: Brightness.dark,
-				),
-			),
-			home: const Screen(title: 'Calculator'),
-		);
-	}
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Calculator App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.blueGrey,
+          brightness: Brightness.dark,
+        ),
+      ),
+      home: const Screen(title: 'Calculator'),
+    );
+  }
 }
 
-class CalculatorButton extends StatelessWidget
-{
-	CalculatorButton({
-		super.key,
-		required this.buttonText,
-		required this.action,
-	});
-	final String buttonText;
-	final void Function(String) action;
+class CalculatorButton extends StatelessWidget {
+  const CalculatorButton({
+    super.key,
+    required this.buttonText,
+    required this.action,
+  });
+  final String buttonText;
+  final void Function(String) action;
 
-	@override
-	Widget build(BuildContext context)
-	{
-		final isPortrait = MediaQuery.of(context).orientation == Orientation.portrait;
-		double padding;
-		double fontSize;
-		Color textColor;
+  double _getPadding(BuildContext context) {
+    final Orientation orientation;
 
-		if (isPortrait)
-		{
-			padding = 15;
-			fontSize = 30;
-		}
-		else
-		{
-			padding = 10;
-			fontSize = 20;
-		}
-		if (buttonText == '=' || buttonText == '+' || buttonText == '-'
-					|| buttonText == 'x' || buttonText == '/')
-		{
-			textColor = Colors.white;
-		}
-		else if (buttonText == 'C' || buttonText == 'AC')
-		{
-			textColor = Colors.red[900]!;
-		}
-		else
-		{
-			textColor = Colors.blueGrey[900]!;
-		}
-		return Expanded(
-			child: TextButton(
-				style: TextButton.styleFrom(
-					padding: EdgeInsets.all(padding),
-					shape: RoundedRectangleBorder(
-						borderRadius: BorderRadius.zero,
-					),
-				),
-				onPressed: ()
-				{
-					debugPrint(buttonText);
-					action(buttonText);
-				},
-				child: Text(
-					buttonText,
-					style: TextStyle(
-						fontSize: fontSize,
-						color: textColor,
-					),
-				),
-			),
-		);
-	}
+    orientation = MediaQuery.of(context).orientation;
+    if (orientation == Orientation.portrait) {
+      return (15);
+    }
+    return (10);
+  }
+
+  double _getFontSize(BuildContext context) {
+    final Orientation orientation;
+
+    orientation = MediaQuery.of(context).orientation;
+    if (orientation == Orientation.portrait) {
+      return (30);
+    }
+    return (20);
+  }
+
+  Color _getTextColor(BuildContext context) {
+    final Map<String, Color> buttonColorMap;
+
+    buttonColorMap = {
+      '=': Colors.white,
+      '+': Colors.white,
+      '-': Colors.white,
+      'x': Colors.white,
+      '/': Colors.white,
+      'C': Colors.red[900]!,
+      'AC': Colors.red[900]!,
+    };
+    if (buttonColorMap[buttonText] != null) {
+      return (buttonColorMap[buttonText]!);
+    }
+    return (Colors.blueGrey[900]!);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final double padding = _getPadding(context);
+    final double fontSize = _getFontSize(context);
+    final Color textColor = _getTextColor(context);
+    return Expanded(
+      child: TextButton(
+        style: TextButton.styleFrom(
+          padding: EdgeInsets.all(padding),
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.zero),
+        ),
+        onPressed: () {
+          debugPrint(buttonText);
+          action(buttonText);
+        },
+        child: Text(
+          buttonText,
+          style: TextStyle(fontSize: fontSize, color: textColor),
+        ),
+      ),
+    );
+  }
 }
 
-class Screen extends StatefulWidget
-{
-	const Screen({super.key, required this.title});
-	final String title;
+class Screen extends StatefulWidget {
+  const Screen({super.key, required this.title});
+  final String title;
 
-	@override
-	State<Screen> createState() => _ScreenState();
+  @override
+  State<Screen> createState() => _ScreenState();
 }
 
-class _ScreenState extends State<Screen>
-{
-	final TextEditingController _expression = TextEditingController(text: '0');
-	final TextEditingController _result = TextEditingController(text: '0');
+class _ScreenState extends State<Screen> {
+  final TextEditingController _expression = TextEditingController(text: '0');
+  final TextEditingController _result = TextEditingController(text: '0');
 
-	void _buttonAction(String buttonText)
-	{
-			setState(()
-			{
-				String expressionText = _expression.text;
-				String resultText = _result.text;
+  void _cleanExpression() {
+    _expression.text = '0';
+    _result.text = '0';
+  }
 
-				if (buttonText == 'AC')
-				{
-						expressionText = '0';
-						resultText = '0';
-				}
-				else if (buttonText == 'C')
-				{
-						if (expressionText.isNotEmpty && expressionText.length > 1)
-						{
-							expressionText = expressionText.substring(0, expressionText.length - 1);
-						}
-						else
-						{
-							expressionText = '0';
-						}
-				}
-				else
-				{
-						if (expressionText == '0')
-						{
-								expressionText = buttonText;
-						}
-						else
-						{
-								expressionText += buttonText;
-						}
-				}
+  void _deleteLastExpressionChar() {
+    String expression;
+    int len;
 
-				_expression.text = expressionText;
-				_result.text = resultText;
-		});
-	}
+    expression = _expression.text;
+    len = expression.length;
+    if (expression.isNotEmpty && len > 1) {
+      expression = expression.substring(0, len - 1);
+    } else {
+      expression = '0';
+    }
+    _expression.text = expression;
+  }
 
-	@override
-	Widget build(BuildContext context)
-	{
-		return Scaffold(
-			appBar: AppBar(
-				backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-				title: Text(widget.title),
-				centerTitle: true,
-			),
-			body: Center(
-				child: Column(
-						mainAxisAlignment: MainAxisAlignment.end,
-						children: [
-						TextField(
-							controller: _expression,
-							decoration: const InputDecoration(
-							enabled: false,
-							border: InputBorder.none,
-							contentPadding: EdgeInsets.only(right: 16.0),
-							),
-							textAlign: TextAlign.right,
-							style: const TextStyle(
-							color: Colors.white,
-							fontSize: 30,
-							),
-						),
-						TextField(
-							controller: _result,
-							decoration: const InputDecoration(
-							enabled: false,
-							border: InputBorder.none,
-							contentPadding: EdgeInsets.only(right: 16.0),
-							),
-							textAlign: TextAlign.right,
-							style: const TextStyle(
-							color: Colors.white,
-							fontSize: 30,
-							),
-						),
-						const Spacer(),
-						Container(
-							color: Colors.blueGrey,
-							child: Row(
-							mainAxisAlignment: MainAxisAlignment.center,
-							children: [
-									CalculatorButton(buttonText: "7", action: _buttonAction),
-									CalculatorButton(buttonText: "8", action: _buttonAction),
-									CalculatorButton(buttonText: "9", action: _buttonAction),
-									CalculatorButton(buttonText: "C", action: _buttonAction),
-									CalculatorButton(buttonText: "AC", action: _buttonAction),
-								],
-							),
-						),
-						Container(
-							color: Colors.blueGrey,
-							child: Row(
-							mainAxisAlignment: MainAxisAlignment.center,
-							children: [
-									CalculatorButton(buttonText: "4", action: _buttonAction),
-									CalculatorButton(buttonText: "5", action: _buttonAction),
-									CalculatorButton(buttonText: "6", action: _buttonAction),
-									CalculatorButton(buttonText: "+", action: _buttonAction),
-									CalculatorButton(buttonText: "-", action: _buttonAction),
-								],
-							),
-						),
-						Container(
-							color: Colors.blueGrey,
-							child: Row(
-							mainAxisAlignment: MainAxisAlignment.center,
-							children: [
-									CalculatorButton(buttonText: "1", action: _buttonAction),
-									CalculatorButton(buttonText: "2", action: _buttonAction),
-									CalculatorButton(buttonText: "3", action: _buttonAction),
-									CalculatorButton(buttonText: "x", action: _buttonAction),
-									CalculatorButton(buttonText: "/", action: _buttonAction),
-								],
-							),
-						),
-						Container(
-							color: Colors.blueGrey,
-							child: Row(
-							mainAxisAlignment: MainAxisAlignment.center,
-							children: [
-									CalculatorButton(buttonText: "0", action: _buttonAction),
-									CalculatorButton(buttonText: ".", action: _buttonAction),
-									CalculatorButton(buttonText: "00", action: _buttonAction),
-									CalculatorButton(buttonText: "=", action: _buttonAction),
-									const Expanded(child: SizedBox()),
-								],
-							),
-						),
-					],
-				),
-			),
-		);
-	}
+  String _getLastNumber(String expression) {
+    int lastOpIndex;
+    String lastNumber;
+
+    lastOpIndex = expression.lastIndexOf(RegExp(r'[+\-x/]'));
+    if (lastOpIndex == -1) {
+      lastNumber = expression;
+    } else {
+      lastNumber = expression.substring(lastOpIndex + 1);
+    }
+    return (lastNumber);
+  }
+
+  bool _isLastNumberDecimal(String expression) {
+    String lastNumber;
+
+    lastNumber = _getLastNumber(expression);
+    return (lastNumber.contains("."));
+  }
+
+  bool _isLastNumberLeadingZero(String expression) {
+    String lastNumber;
+
+    lastNumber = _getLastNumber(expression);
+    return (lastNumber == '0');
+  }
+
+  bool _isDuplicateOperator(String expression, String buttonText) {
+    if (['+', 'x', '/'].contains(buttonText) && expression.endsWith(buttonText)
+        || expression == '-' && buttonText == '-') {
+      return (true);
+    }
+    return (false);
+  }
+
+  String _addExpressionOperator(String expression, String buttonText) {
+    if (_isDuplicateOperator(expression, buttonText)) {
+      return (expression);
+    }
+    if ((['+', 'x', '/'].contains(buttonText) && expression == '-')) {
+      expression = '0';
+    } else if ((expression.endsWith('x-') ||
+            expression.endsWith('/-') ||
+            expression.endsWith('x+') ||
+            expression.endsWith('/+') ||
+            expression.endsWith('--') ||
+            expression.endsWith('+-') ||
+            expression.endsWith('-+')) &&
+        ['x', '/'].contains(buttonText)) {
+      expression = expression.substring(0, expression.length - 2) + buttonText;
+    } else if (((expression.endsWith('+') ||
+                expression.endsWith('-') ||
+                expression.endsWith('x') ||
+                expression.endsWith('/')) &&
+            ['x', '/'].contains(buttonText)) ||
+        ((expression.endsWith('x-') ||
+                expression.endsWith('/-') ||
+                expression.endsWith('x+') ||
+                expression.endsWith('/+') ||
+                expression.endsWith('--') ||
+                expression.endsWith('+-') ||
+                expression.endsWith('-+')) &&
+            ['+', '-'].contains(buttonText))) {
+      expression = expression.substring(0, expression.length - 1) + buttonText;
+    } else if (expression == '0' && buttonText == '-') {
+      expression = buttonText;
+    } else {
+      expression += buttonText;
+    }
+    return (expression);
+  }
+
+  String _addExpressionDecimalPoint(String expression, String buttonText) {
+    if (buttonText == "." && _isLastNumberDecimal(expression)) {
+      return (expression);
+    }
+    if ((expression.endsWith('+') ||
+        expression.endsWith('-') ||
+        expression.endsWith('x') ||
+        expression.endsWith('/'))) {
+      expression = '${expression}0$buttonText';
+    } else {
+      expression += buttonText;
+    }
+    return (expression);
+  }
+
+  String _addExpressionDoubleZero(String expression, String buttonText) {
+    if (['00', '0'].contains(buttonText) &&
+        _isLastNumberLeadingZero(expression)) {
+      return (expression);
+    }
+    if ((expression.endsWith('+') ||
+        expression.endsWith('-') ||
+        expression.endsWith('x') ||
+        expression.endsWith('/'))) {
+      expression = '${expression}0';
+    } else {
+      expression += buttonText;
+    }
+    return (expression);
+  }
+
+  String _addExpressionNumber(String expression, String buttonText) {
+    if (expression == '0' ||
+        expression.endsWith('+0') ||
+        expression.endsWith('-0') ||
+        expression.endsWith('x0') ||
+        expression.endsWith('/0')) {
+      expression = expression.substring(0, expression.length - 1) + buttonText;
+    } else {
+      expression += buttonText;
+    }
+    return (expression);
+  }
+
+  void _addExpressionText(String buttonText) {
+    String expression;
+
+    expression = _expression.text;
+    switch (buttonText) {
+      case '+':
+      case '-':
+      case 'x':
+      case '/':
+        expression = _addExpressionOperator(expression, buttonText);
+        break;
+      case '.':
+        expression = _addExpressionDecimalPoint(expression, buttonText);
+        break;
+      case '00':
+        expression = _addExpressionDoubleZero(expression, buttonText);
+        break;
+      default:
+        expression = _addExpressionNumber(expression, buttonText);
+        break;
+    }
+    _expression.text = expression;
+  }
+
+  void	_calcExpression()
+  {
+    String expression;
+
+    expression = _expression.text.replaceAll('0.+', '0+')
+                                 .replaceAll('0.-', '0-')
+                                 .replaceAll('0.x', '0x')
+                                 .replaceAll('0./', '/');
+    _expression.text = expression;
+  }
+
+  void _buttonAction(String buttonText) {
+    setState(() {
+      switch (buttonText) {
+        case 'AC':
+          _cleanExpression();
+          break;
+        case 'C':
+          _deleteLastExpressionChar();
+          break;
+        case '=':
+          _calcExpression();
+          break;
+        default:
+          _addExpressionText(buttonText);
+          break;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            TextField(
+              controller: _expression,
+              decoration: const InputDecoration(
+                enabled: false,
+                border: InputBorder.none,
+                contentPadding: EdgeInsets.only(right: 16.0),
+              ),
+              textAlign: TextAlign.right,
+              style: const TextStyle(color: Colors.white, fontSize: 30),
+            ),
+            TextField(
+              controller: _result,
+              decoration: const InputDecoration(
+                enabled: false,
+                border: InputBorder.none,
+                contentPadding: EdgeInsets.only(right: 16.0),
+              ),
+              textAlign: TextAlign.right,
+              style: const TextStyle(color: Colors.white, fontSize: 30),
+            ),
+            const Spacer(),
+            Container(
+              color: Colors.blueGrey,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CalculatorButton(buttonText: "7", action: _buttonAction),
+                  CalculatorButton(buttonText: "8", action: _buttonAction),
+                  CalculatorButton(buttonText: "9", action: _buttonAction),
+                  CalculatorButton(buttonText: "C", action: _buttonAction),
+                  CalculatorButton(buttonText: "AC", action: _buttonAction),
+                ],
+              ),
+            ),
+            Container(
+              color: Colors.blueGrey,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CalculatorButton(buttonText: "4", action: _buttonAction),
+                  CalculatorButton(buttonText: "5", action: _buttonAction),
+                  CalculatorButton(buttonText: "6", action: _buttonAction),
+                  CalculatorButton(buttonText: "+", action: _buttonAction),
+                  CalculatorButton(buttonText: "-", action: _buttonAction),
+                ],
+              ),
+            ),
+            Container(
+              color: Colors.blueGrey,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CalculatorButton(buttonText: "1", action: _buttonAction),
+                  CalculatorButton(buttonText: "2", action: _buttonAction),
+                  CalculatorButton(buttonText: "3", action: _buttonAction),
+                  CalculatorButton(buttonText: "x", action: _buttonAction),
+                  CalculatorButton(buttonText: "/", action: _buttonAction),
+                ],
+              ),
+            ),
+            Container(
+              color: Colors.blueGrey,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CalculatorButton(buttonText: "0", action: _buttonAction),
+                  CalculatorButton(buttonText: ".", action: _buttonAction),
+                  CalculatorButton(buttonText: "00", action: _buttonAction),
+                  CalculatorButton(buttonText: "=", action: _buttonAction),
+                  const Expanded(child: SizedBox()),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
--- mobileModule00/calculator_app/test/widget_test.dart
+++ mobileModule00/calculator_app/test/widget_test.dart
@@ -1,30 +1,33 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:calculator_app/main.dart';
+import 'package:flutter/material.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  testWidgets('Calculator app has a title', (WidgetTester tester) async {
+    await tester.pumpWidget(const CalculatorApp());
+    expect(find.text('Calculator'), findsOneWidget);
+  });
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+  testWidgets('Calculator app has buttons', (WidgetTester tester) async {
+    await tester.pumpWidget(const CalculatorApp());
+    expect(find.widgetWithText(TextButton, '7'), findsOneWidget);
+    expect(find.widgetWithText(TextButton, '8'), findsOneWidget);
+    expect(find.widgetWithText(TextButton, '9'), findsOneWidget);
+    expect(find.widgetWithText(TextButton, 'C'), findsOneWidget);
+    expect(find.widgetWithText(TextButton, 'AC'), findsOneWidget);
+    expect(find.widgetWithText(TextButton, '4'), findsOneWidget);
+    expect(find.widgetWithText(TextButton, '5'), findsOneWidget);
+    expect(find.widgetWithText(TextButton, '6'), findsOneWidget);
+    expect(find.widgetWithText(TextButton, '+'), findsOneWidget);
+    expect(find.widgetWithText(TextButton, '-'), findsOneWidget);
+    expect(find.widgetWithText(TextButton, '1'), findsOneWidget);
+    expect(find.widgetWithText(TextButton, '2'), findsOneWidget);
+    expect(find.widgetWithText(TextButton, '3'), findsOneWidget);
+    expect(find.widgetWithText(TextButton, 'x'), findsOneWidget);
+    expect(find.widgetWithText(TextButton, '/'), findsOneWidget);
+    expect(find.widgetWithText(TextButton, '0'), findsOneWidget);
+    expect(find.widgetWithText(TextButton, '.'), findsOneWidget);
+    expect(find.widgetWithText(TextButton, '00'), findsOneWidget);
+    expect(find.widgetWithText(TextButton, '='), findsOneWidget);
   });
 }
