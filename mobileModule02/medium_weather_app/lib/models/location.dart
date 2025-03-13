0baa424e6c2c801ddd27e8b55c9595761073556c
Jonatas Arão de Carvalho Neves <51247074+JonatasArao@users.noreply.github.com>
2025-03-13 16:12:29 -0300

    mobileModule02 - Searcher - getting data from http

--- mobileModule02/medium_weather_app/lib/models/location.dart
+++ mobileModule02/medium_weather_app/lib/models/location.dart
@@ -1,12 +1,25 @@
 import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class Location {
   final double _latitude;
   final double _longitude;
+  final String? _name;
+  final String? _region;
+  final String? _country;
 
-  const Location._({required double latitude, required double longitude})
-    : _latitude = latitude,
-      _longitude = longitude;
+  const Location._({
+    required double latitude,
+    required double longitude,
+    String? name,
+    String? region,
+    String? country,
+  }) : _latitude = latitude,
+       _longitude = longitude,
+       _name = name,
+       _region = region,
+       _country = country;
 
   factory Location._fromGeolocation(Position position) {
     return Location._(
@@ -15,8 +28,25 @@ class Location {
     );
   }
 
+  factory Location._fromJson(Map<String, dynamic> json) {
+    try {
+    return Location._(
+        latitude: json['latitude'] as double,
+        longitude: json['longitude'] as double,
+        name: json['name'] as String?,
+        region: json['admin1'] as String?,
+        country: json['country'] as String?,
+      );
+    } catch (e) {
+      throw FormatException('Failed to load location: $json');
+    }
+  }
+
   double get latitude => _latitude;
   double get longitude => _longitude;
+  String? get name => _name;
+  String? get region => _region;
+  String? get country => _country;
 
   static Future<Location> fetchGeolocation() async {
     Location location;
@@ -45,6 +75,20 @@ class Location {
       return (location);
     } catch (e) {
       throw e.toString();
+    }
+  }
+
+  static Future<List<Location>> fetchLocations(String name) async {
+    final response = await http.get(
+      Uri.parse('https://geocoding-api.open-meteo.com/v1/search?name=$name'),
+    );
+
+    if (response.statusCode == 200) {
+      Map<String, dynamic> jsonResponse = jsonDecode(response.body) as Map<String, dynamic>;
+      List<dynamic> jsonList = jsonResponse['results'] as List<dynamic>;
+      return jsonList.map((json) => Location._fromJson(json as Map<String, dynamic>)).toList();
+    } else {
+      throw Exception('Failed to load locations');
     }
   }
 }
